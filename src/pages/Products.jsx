dedf6f0c349b800700bf2e55fa298da7fe0368ed
Reza Hajibabaei <rezahajibabaei2001@gmail.com>
2025-12-26 22:53:00 +0330

    Create a products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { getData } from "../context/DataContext";
+import FilterSection from "../components/FilterSection";
+import Loading from "../assets/images/Loading4.webm";
+import ProductCard from "../components/ProductCard";
 
 const Products = () => {
+  const { data, fetchAllProducts } = getData();
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
   return (
-    <div>Products</div>
-  )
-}
+    <div>
+      <div className="max-w-6xl mx-auto px--4 mb-10">
+        {data?.length > 0 ? (
+          <div className="flex gap-8">
+            <FilterSection />
+            <div className="grid grid-cols-4 gap-7 mt-10">
+              {data?.map((product, index) => {
+                return <ProductCard key={index} product={product} />;
+              })}
+            </div>
+          </div>
+        ) : (
+          <div className="flex items-center justify-center h-100">
+            <video muted autoPlay loop>
+              <source src={Loading} type="video/webm" />
+            </video>
+          </div>
+        )}
+      </div>
+      
+    </div>
+  );
+};
+console.log("hii")
 
-export default Products
+export default Products;
