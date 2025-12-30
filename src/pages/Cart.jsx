b282a7658928bfaf7ef008a0027974736f92c789
Reza Hajibabaei <rezahajibabaei2001@gmail.com>
2025-12-30 23:31:18 +0330

    Making the shopping cart page responsive for phone sizes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -15,7 +15,7 @@ const Cart = ({ location, getLocation }) => {
   // محاسبه جمع قیمت محصولات
   const totalPrice = cartItem.reduce((total, item) => total + item.price, 0);
   return (
-    <div className="mt-10 max-w-6xl mx-auto mb-5 text-right md:px-8">
+    <div className="mt-10 max-w-6xl mx-auto mb-5 text-right px-4 md:px-0">
       {cartItem.length > 0 ? (
         <div>
           <h1 className="font-Vazir-Bold text-2xl">
@@ -30,7 +30,7 @@ const Cart = ({ location, getLocation }) => {
                     key={index}
                     className="bg-gray-100 p-5 rounded-md flex justify-end items-center mt-3"
                   >
-                    <div className="flex items-center gap-10">
+                    <div className="flex items-center gap-2 md:gap-10">
                       <span
                         onClick={() => deleteItem(item.id)}
                         className="hover:bg-white/60 transition-all rounded-full p-3 hover:shadow-2xl"
@@ -57,7 +57,7 @@ const Cart = ({ location, getLocation }) => {
                         </button>
                       </div>
                       <div className="text-right">
-                        <h1 className="w-75 line-clamp-2">{item.title}</h1>
+                        <h1 className="md:w-75 line-clamp-2">{item.title}</h1>
                         <p className="text-red-500 font-semibold text-lg">
                           ${item.price}
                         </p>
@@ -73,7 +73,7 @@ const Cart = ({ location, getLocation }) => {
               })}
             </div>
             {/* بخش فاکتور */}
-            <div className="grid grid-cols-2 gap-20">
+            <div className="grid grid-cols-1 md:grid-cols-2 md:gap-20">
               <div className="bg-white border border-gray-100 shadow-xl rounded-md p-7 mt-4 space-y-2 h-max">
                 <h1 className="text-gray-800 font-Vazir-Bold text-xl">
                   فاکتور فروش
