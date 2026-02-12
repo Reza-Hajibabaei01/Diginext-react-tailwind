fb0dbea8e61f0d44c19dbbe55fe0d43ea461322b
Reza Hajibabaei <rezahajibabaei2001@gmail.com>
2026-02-12 20:02:30 +0330

    Setting the product page to be responsive

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -14,13 +14,13 @@ function ProductCard({ product }) {
         className="bg-gray-100 aspect-square"
         onClick={() => navigate(`/products/${product.id}`)}
       />
-      <h1 className="line-clamp-2 p-1 font-Vazir-Normal">{product.title}</h1>
+      <h1 className="line-clamp-1 p-1 font-Vazir-Normal">{product.title}</h1>
       <p className="my-1 text-lg text-gray-800 font-bold">${product.price}</p>
       <button
         onClick={() => addToCart(product)}
-        className="bg-red-500 px-1 md:px-3 py-2 text-[12px] md:text-sm rounded-md text-white w-full cursor-pointer flex gap-1 md:gap-2 items-center justify-center font-Vazir-Medium"
+        className="bg-red-500 xl:text-base lg:text-[12px] lg:px-2 lg:py-2 md:text-[12px] md:px-2 md:py-1 px-1 py-2 rounded-md text-white w-full cursor-pointer flex gap-1  items-center justify-center font-Vazir-Medium"
       >
-        <IoCartOutline className="md:w-6 md:h-6 w-4 h-4" />
+        <IoCartOutline className="h-5 w-5" />
         افزودن به سبد خرید
       </button>
     </div>
